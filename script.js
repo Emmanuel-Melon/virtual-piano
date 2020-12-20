5642508e3cbbfc421048724d9550c2c54306389b
Eman <emmanuelgatwech@gmail.com>
2020-12-21 01:39:41 +0300

    add piano assests and restructure styles

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const kbd = document.querySelector("kbd");
+const kbd = document.querySelectorAll("kbd");
+
+Array.from(kbd).forEach(el => {
+  el.addEventListener("click", e => {
+    const { target } = e;
+    e.target.classList.add("pressed");
+    document.getElementById(`${target.innerText.toLocaleLowerCase()}`).play();
+  });
+
+});
